031496c8c9dff27669ebae42bff5b1baa1046742
Connor Davis <davisc@ainfosec.com>
2019-07-09 10:40:23 -0600

    Update fakeroot.sh
    Guests are not build with modules, so disable the modules-load
    systemd service.
    
    Add /proc/xen to /etc/fstab so that the control_d is avaiable for
    xenstore.

--- deps/buildroot/fakeroot.sh
+++ deps/buildroot/fakeroot.sh
@@ -2,6 +2,7 @@
 
 # $1 == the path to the rootfs
 
+rm -rf $1/usr/lib/systemd/system/systemd-modules-load.service
 rm -rf $1/usr/lib/systemd/system/systemd-update-utmp.service
 rm -rf $1/usr/lib/systemd/system/systemd-update-utmp-runlevel.service
 rm -rf $1/usr/lib/systemd/system/sysinit.target.wants/systemd-update-utmp.service
@@ -9,3 +10,5 @@ rm -rf $1/usr/lib/systemd/system/sysinit.target.wants/systemd-update-utmp-runlev
 
 rm -rf $1/etc/systemd/system/network.service
 rm -rf $1/etc/systemd/system/multi-user.target.wants/network.service
+
+echo '/proc/xen /proc/xen xenfs' >> $1/etc/fstab
