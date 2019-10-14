4e27fd794fa223700b79c12cdec5df1070b56cf1
Connor Davis <davisc@ainfosec.com>
2019-10-14 13:57:05 -0600

    winpv/xenbus: Build for Win10 x64 by default
    Rename 'Build' to 'Win8Build' and add 'Win10Build' in build.ps1.
    Use Win10Build x64 as the default build command.

--- drivers/winpv/xenbus/build.ps1
+++ drivers/winpv/xenbus/build.ps1
@@ -12,7 +12,7 @@ param(
 # Script Body
 #
 
-Function Build {
+Function Win8Build {
 	param(
 		[string]$Arch,
 		[string]$Type
@@ -21,6 +21,25 @@ Function Build {
 	$visualstudioversion = $Env:VisualStudioVersion
 	$solutiondir = @{ "14.0" = "vs2015"; "15.0" = "vs2017"; "16.0" = "vs2019"; }
 	$configurationbase = @{ "14.0" = "Windows 8"; "15.0" = "Windows 8"; "16.0" = "Windows 8"; }
+
+	$params = @{
+		SolutionDir = $solutiondir[$visualstudioversion];
+		ConfigurationBase = $configurationbase[$visualstudioversion];
+		Arch = $Arch;
+		Type = $Type
+		}
+	& ".\msbuild.ps1" @params
+}
+
+Function Win10Build {
+	param(
+		[string]$Arch,
+		[string]$Type
+	)
+
+	$visualstudioversion = $Env:VisualStudioVersion
+	$solutiondir = @{ "14.0" = "vs2015"; "15.0" = "vs2017"; "16.0" = "vs2019"; }
+	$configurationbase = @{ "14.0" = "Windows 10"; "15.0" = "Windows 10"; "16.0" = "Windows 10"; }
 
 	$params = @{
 		SolutionDir = $solutiondir[$visualstudioversion];
@@ -78,8 +97,7 @@ Set-Item -Path Env:MAJOR_VERSION -Value '9'
 Set-Item -Path Env:MINOR_VERSION -Value '0'
 Set-Item -Path Env:MICRO_VERSION -Value '0'
 
-Build "x86" $Type
-Build "x64" $Type
+Win10Build "x64" $Type
 
 if ($Sdv) {
 	SdvBuild
