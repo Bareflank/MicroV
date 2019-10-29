4d88ba06494e6cc6e105376bd88788a71eba197a
Connor Davis <davisc@ainfosec.com>
2019-10-29 12:07:39 -0600

    winpv/xennet: Build for Win10 x64 by default
    Rename 'Build' to 'Win8Build' and add 'Win10Build' in build.ps1.
    Use Win10Build x64 as the default build command.

--- drivers/winpv/xennet/build.ps1
+++ drivers/winpv/xennet/build.ps1
@@ -12,7 +12,7 @@ param(
 # Script Body
 #
 
-Function Build {
+Function Win8Build {
 	param(
 		[string]$Arch,
 		[string]$Type
@@ -31,9 +31,28 @@ Function Build {
 	& ".\msbuild.ps1" @params
 }
 
+Function Win10Build {
+	param(
+		[string]$Arch,
+		[string]$Type
+	)
+
+	$visualstudioversion = $Env:VisualStudioVersion
+	$solutiondir = @{ "14.0" = "vs2015"; "15.0" = "vs2017"; "16.0" = "vs2019"; }
+	$configurationbase = @{ "14.0" = "Windows 10"; "15.0" = "Windows 10"; "16.0" = "Windows 10"; }
+
+	$params = @{
+		SolutionDir = $solutiondir[$visualstudioversion];
+		ConfigurationBase = $configurationbase[$visualstudioversion];
+		Arch = $Arch;
+		Type = $Type
+		}
+	& ".\msbuild.ps1" @params
+}
+
 Function SdvBuild {
 	$visualstudioversion = $Env:VisualStudioVersion
-	$solutiondir = @{ "14.0" = "vs2015"; "15.0" = "vs2017"; "16.0" = "vs2017"; }
+	$solutiondir = @{ "14.0" = "vs2015"; "15.0" = "vs2017"; "16.0" = "vs2019"; }
 	$configurationbase = @{ "14.0" = "Windows 10"; "15.0" = "Windows 10"; "16.0" = "Windows 10"; }
 	$arch = "x64"
 
@@ -78,8 +97,10 @@ Set-Item -Path Env:MAJOR_VERSION -Value '9'
 Set-Item -Path Env:MINOR_VERSION -Value '0'
 Set-Item -Path Env:MICRO_VERSION -Value '0'
 
-Build "x86" $Type
-Build "x64" $Type
+$redist = $PSScriptRoot + "\..\..\..\deploy\windows\redist"
+Set-Item -Path Env:DPINST_REDIST -Value $redist
+
+Win10Build "x64" $Type
 
 if ($Sdv) {
 	SdvBuild
