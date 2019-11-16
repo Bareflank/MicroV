0cf4779b3db45356e2ab35370b97d604f94572e2
Connor Davis <davisc@ainfosec.com>
2019-11-15 22:24:10 -0700

    winpv/xenvif: Allow TOEPLITZ hashing with one queue

--- drivers/winpv/xenvif/src/xenvif/frontend.c
+++ drivers/winpv/xenvif/src/xenvif/frontend.c
@@ -1941,11 +1941,9 @@ FrontendSetHashAlgorithm(
         break;
 
     case XENVIF_PACKET_HASH_ALGORITHM_TOEPLITZ:
-        // Don't allow toeplitz hashing to be configured for a single
-        // queue, or if it has been explicitly disabled
+        // Don't allow toeplitz hashing if it has been explicitly disabled
         ASSERT(__FrontendGetNumQueues(Frontend) != 0);
-        status = (__FrontendGetNumQueues(Frontend) == 1 ||
-                  Frontend->DisableToeplitz != 0) ?
+        status = (Frontend->DisableToeplitz != 0) ?
                  STATUS_NOT_SUPPORTED :
                  STATUS_SUCCESS;
         break;
