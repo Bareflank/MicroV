9bae3b3cb2ff43d6c03e0e42f20e7c79542d9a07
Connor Davis <davisc@ainfosec.com>
2020-01-10 15:26:09 -0700

    deploy: Execute startvms script and increase xsvm memory

--- deploy/windows/scripts/startvms.ps1
+++ deploy/windows/scripts/startvms.ps1
@@ -20,4 +20,4 @@
 # SOFTWARE.
 
 cd 'C:\Program Files\Beam'
-Start-Process -FilePath .\uvctl.exe -ArgumentList '--ram 300000000 --xsvm --cmdline "xen-pciback.hide=(00:14.3)(01:00.0) xen-pciback.passthrough=1" --kernel .\xsvm-vmlinux --initrd .\xsvm-rootfs.cpio.gz' -NoNewWindow -RedirectStandardOutput 'C:\Program Files\Beam\logs\uvctl-out-log.txt' -RedirectStandardError 'C:\Program Files\Beam\logs\uvctl-err-log.txt'
+Start-Process -FilePath .\uvctl.exe -ArgumentList '--ram 350000000 --xsvm --cmdline "xen-pciback.hide=(00:14.3)(01:00.0) xen-pciback.passthrough=1" --kernel .\xsvm-vmlinux --initrd .\xsvm-rootfs.cpio.gz' -NoNewWindow -RedirectStandardOutput 'C:\Program Files\Beam\logs\uvctl-out-log.txt' -RedirectStandardError 'C:\Program Files\Beam\logs\uvctl-err-log.txt'
