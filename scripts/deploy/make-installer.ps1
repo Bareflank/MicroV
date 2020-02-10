3a4c569713ab088988a5e10c774c917d5ce26eb7
Connor Davis <davisc@ainfosec.com>
2020-02-09 21:44:27 -0700

    scripts: update make-installer.ps1
    - Make the first argument "ProductName" mandatory. This is used as
      the product name by the inno setup script app.iss.
    - Add a function to parse the CN of the WDK cert used to sign the
      builder and visr drivers. This is passed as a /D option to the
      Inno Setup Preprocessor to avoid hardcoding it in app.iss.
    - Invoke copy-drivers.ps1 prior to invoking iscc.

--- scripts/deploy/make-installer.ps1
+++ scripts/deploy/make-installer.ps1
@@ -20,18 +20,42 @@
 # SOFTWARE.
 
 param(
-    [string]$IssPath,
-    [string]$Name
+    [parameter(mandatory)] [string] $ProductName,
+    [string] $IssPath
 )
+
+function parse-wdk-cert-cn {
+    # extract the CN from the WDK test cert used to sign builder and visr
+    $deploy = "$PSScriptRoot\..\..\deploy\windows\drivers"
+    $matchinfo = certutil -dump $deploy\builder\builder.cer | Select-String -CaseSensitive -Pattern "CN=.+,"
+
+    if ($matchinfo) {
+        $line = $matchinfo[0].Line
+
+        # sanity check to ensure each match is the same (i.e. one CN per cert)
+        for ($i = 1; $i -lt $matchinfo.Length; $i++) {
+            if ($line -ne $matchinfo[$i].Line) {
+                Throw
+            }
+        }
+
+        $name = $line.Split(',')[0]
+        $name = $name.Split('=')[1]
+
+        Write-Host "Using WDK cert with CN = $name"
+        Return $name
+    } else {
+        Throw "Unable to extract CN from builder cert"
+    }
+}
 
 if ([string]::IsNullOrEmpty($IssPath)) {
     $IssPath = "$PSScriptRoot\..\..\deploy\windows\app.iss"
 }
 
-if ([string]::IsNullOrEmpty($Name)) {
-    $Name = 'install-beam'
-}
-
+$name_lower = $ProductName.ToLower()
+$name_title = (Get-Culture).TextInfo.ToTitleCase($ProductName)
+$installer = "install-$name_lower"
 $iscc = 'C:\Program Files (x86)\Inno Setup 6\iscc.exe'
 
 if (!(Test-Path $iscc)) {
@@ -42,5 +66,13 @@ if (!(Test-Path $IssPath)) {
     throw "ERROR: Invalid path to iss file: $IssPath"
 }
 
-$cmd = "& `'$iscc`' /F`"$Name`" $IssPath"
+& $PSScriptRoot\copy-drivers.ps1 | Out-Null
+$wdk_cert_cn = parse-wdk-cert-cn
+
+$cmd = "& `'$iscc`' /F`"$installer`" "
+$cmd = $cmd + "`"/DNAME_LOWER=$name_lower`" "
+$cmd = $cmd + "`"/DNAME_TITLE=$name_title`" "
+$cmd = $cmd + "`"/DWDK_CERT_CN=$wdk_cert_cn`" "
+$cmd = $cmd + "`"$IssPath`""
+
 iex $cmd
