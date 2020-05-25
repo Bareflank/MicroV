13858e8f0be75688755a180d6cc1a96c758ac199
Connor Davis <davisc@ainfosec.com>
2020-05-25 11:58:12 -0600

    drivers/winpv: Fix bug in GnttabQueryReference
    Use (flags & GTF_readonly) instead of (frame & GTF_readonly) when
    checking if the reference is read-only.

--- drivers/winpv/xenbus/src/xenbus/gnttab.c
+++ drivers/winpv/xenbus/src/xenbus/gnttab.c
@@ -604,7 +604,7 @@ GnttabQueryReference(
         *Pfn = Context->Table[Reference].frame;
 
     if (ReadOnly != NULL)
-        *ReadOnly = (Context->Table[Reference].frame & GTF_readonly) ? TRUE : FALSE;
+        *ReadOnly = (Context->Table[Reference].flags & GTF_readonly) ? TRUE : FALSE;
 
     return STATUS_SUCCESS;
 
