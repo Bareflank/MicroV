8e2700663223dc6ecf2a239b6bb62b0dfc0a2463
Pavan <pavan.reddy@infovision.net>
2021-09-14 15:57:16 -0700

    kvm get_regs API

--- shim/src/handle_vcpu_kvm_get_regs.c
+++ shim/src/handle_vcpu_kvm_get_regs.c
@@ -28,8 +28,8 @@
 #include <g_mut_hndl.h>
 #include <kvm_regs.h>
 #include <kvm_regs_idxs.h>
+#include <mv_constants.h>
 #include <mv_hypercall.h>
-#include <mv_rdl_entry_t.h>
 #include <mv_rdl_t.h>
 #include <mv_reg_t.h>
 #include <platform.h>
@@ -42,13 +42,13 @@
  *   @brief Handles the execution of kvm_get_regs.
  *
  * <!-- inputs/outputs -->
- *   @param mut_vcpu arguments received from private data	
+ *   @param pmut_cst_vcpu arguments received from private data	
  *   @param pmut_args the arguments provided by userspace
  *   @return SHIM_SUCCESS on success, SHIM_FAILURE on failure.
  */
 NODISCARD int64_t
 handle_vcpu_kvm_get_regs(
-    struct shim_vcpu_t const *const mut_vcpu, struct kvm_regs *const pmut_args) NOEXCEPT
+    struct shim_vcpu_t const *const pmut_cst_vcpu, struct kvm_regs *const pmut_args) NOEXCEPT
 {
 
     platform_expects(MV_INVALID_HANDLE != g_mut_hndl);
@@ -76,7 +76,7 @@ handle_vcpu_kvm_get_regs(
     pmut_rdl->entries[RFLAGS_IDX].reg = (uint64_t)mv_reg_t_rflags;
     pmut_rdl->num_entries = TOTAL_NUM_ENTRIES;
 
-    if (mv_vs_op_reg_get_list(g_mut_hndl, mut_vcpu->vsid)) {
+    if (mv_vs_op_reg_get_list(g_mut_hndl, pmut_cst_vcpu->vsid)) {
         bferror("ms_vs_op_reg_get_list failed");
         return SHIM_FAILURE;
     }
