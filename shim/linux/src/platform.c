2290364fd443ad349b80c6ff8a00b84fa45cb390
Christopher Pelloux <git@chp.io>
2021-11-02 13:07:30 -0400

    shim: Fix page count off by one

--- shim/linux/src/platform.c
+++ shim/linux/src/platform.c
@@ -285,7 +285,7 @@ platform_mlock(void *const pmut_ptr, uint64_t const num, uintptr_t *pmut_os_info
 {
     struct page **pages = NULL;
     int rc = 0;
-    uint64_t page_count = (num >> HYPERVISOR_PAGE_SHIFT) + 1;
+    uint64_t page_count = (num >> HYPERVISOR_PAGE_SHIFT);
 
     platform_expects(((void *)0) != pmut_ptr);
     platform_expects(((uint64_t)0) != num);
@@ -341,7 +341,7 @@ NODISCARD int64_t
 platform_munlock(void *const pmut_ptr, uint64_t const num, uintptr_t const os_info) NOEXCEPT
 {
     struct page **pages = (struct page **)os_info;
-    uint64_t page_count = (num >> HYPERVISOR_PAGE_SHIFT) + 1;
+    uint64_t page_count = (num >> HYPERVISOR_PAGE_SHIFT);
 
     platform_expects(((void *)0) != pmut_ptr);
     platform_expects(((uint64_t)0) != num);
