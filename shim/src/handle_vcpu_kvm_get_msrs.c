b1141daa13f07b0378a85cc7d450f2f5913b5306
Pavan <pavan.reddy@infovision.net>
2021-11-24 16:17:07 +0000

    MSRS changes

--- shim/src/handle_vcpu_kvm_get_msrs.c
+++ shim/src/handle_vcpu_kvm_get_msrs.c
@@ -24,20 +24,63 @@
  * SOFTWARE.
  */
 
+#include <debug.h>
+#include <detect_hypervisor.h>
+#include <g_mut_hndl.h>
+#include <kvm_msr_entry.h>
 #include <kvm_msrs.h>
+#include <mv_constants.h>
+#include <mv_hypercall.h>
+#include <mv_rdl_t.h>
 #include <mv_types.h>
-
+#include <platform.h>
+#include <shared_page_for_current_pp.h>
+#include <shim_vcpu_t.h>
 /**
  * <!-- description -->
  *   @brief Handles the execution of kvm_get_msrs.
  *
  * <!-- inputs/outputs -->
- *   @param pmut_ioctl_args the arguments provided by userspace
+ *   @param vcpu arguments received from private data
+ *   @param pmut_args the arguments provided by userspace
  *   @return SHIM_SUCCESS on success, SHIM_FAILURE on failure.
  */
 NODISCARD int64_t
-handle_vcpu_kvm_get_msrs(struct kvm_msrs *const pmut_ioctl_args) NOEXCEPT
+handle_vcpu_kvm_get_msrs(
+    struct shim_vcpu_t const *const vcpu, struct kvm_msrs *const pmut_args) NOEXCEPT
 {
-    (void)pmut_ioctl_args;
+    uint64_t mut_i;
+    platform_expects(MV_INVALID_HANDLE != g_mut_hndl);
+    platform_expects(NULL != vcpu);
+    platform_expects(NULL != pmut_args);
+
+    if (detect_hypervisor()) {
+        bferror("The shim is not running in a VM. Did you forget to start MicorV?");
+        return SHIM_FAILURE;
+    }
+
+    struct mv_rdl_t *const pmut_rdl = (struct mv_rdl_t *)shared_page_for_current_pp();
+    platform_expects(NULL != pmut_rdl);
+
+    pmut_rdl->num_entries = (uint64_t)pmut_args->nmsrs;
+
+    for (mut_i = ((uint64_t)0); mut_i < pmut_rdl->num_entries; ++mut_i) {
+        pmut_rdl->entries[mut_i].reg = (uint64_t)pmut_args->entries[mut_i].index;
+    }
+
+    if (mv_vs_op_msr_get_list(g_mut_hndl, vcpu->vsid)) {
+        bferror("mv_vs_op_msr_get_list failed");
+        return SHIM_FAILURE;
+    }
+
+    if (pmut_rdl->num_entries != (uint64_t)pmut_args->nmsrs) {
+        bferror("The RDL's num_entries is no longer valid");
+        return SHIM_FAILURE;
+    }
+
+    for (mut_i = ((uint64_t)0); mut_i < pmut_rdl->num_entries; ++mut_i) {
+        pmut_args->entries[mut_i].data = pmut_rdl->entries[mut_i].val;
+    }
+
     return SHIM_SUCCESS;
 }
